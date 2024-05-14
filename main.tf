ba367c5d4d6e0abdf0c831a248941f840937bcb3
Gowtham Uppalapati <161915120+GowthamUppalapati@users.noreply.github.com>
2024-05-14 21:26:43 +0530

    bucket Name

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_security_group" "webSg" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "Gowthamterraform2024project"
+  bucket = "gowtham-terraform-proj"
 }
 
 resource "aws_instance" "webserver1" {
